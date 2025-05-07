f1d3858c73674fb2b2f878316776439e7ddd2074
Paveljolak <paveljolak123@gmail.com>
2025-05-07 18:55:20 +0200

    Models done.

---
+++ Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace libraryChallenge.Models;
+
+public class Author
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!; // null! -> to shut up null warnings.
+    public DateTime DateOfBirth { get; set; }
+
+    public List<Book> Books { get; set; } = new(); // used to define one-to-many relationship. Do I know why? Noup.
+
+}
---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace libraryChallenge.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public int PublicationYear { get; set; }
+    
+    public int AuthorId { get; set; }
+    public Author Author { get; set; } = null!; // many-to-one relationship. The same as in Author, but now for book.
+    
+}
